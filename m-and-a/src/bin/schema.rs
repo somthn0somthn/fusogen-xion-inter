854a3f28acbd100c0e4c39498131f7b00a1415c4
somthn0somthn <lpmaho@gmail.com>
2025-01-16 03:01:03 +0900

    integrated whitlist sc

---
+++ m-and-a/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use m_and_a::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
